8161dd99d1adb9b11b26e79cb9116fdde1a3293d
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-07-13 15:43:36 -0400

    Noo need for this anymore since we're using a background page

--- js/background.js
+++ js/background.js
@@ -1,7 +1,25 @@
+var baseURL = "http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=mcgill+university&schoolID=&query=";
+
 //display icon when navigating through McGill domains
 chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
     if(tab.url.search("mcgill.ca") > -1)
     {
         chrome.pageAction.show(tabId);
     }
+});
+
+chrome.runtime.onMessage.addListener(function(msg,sender,senderResp){
+    //put request in a function that returns response
+    var query = msg.query;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET",baseURL+query,true);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.statusText === "OK")
+        {
+            //move to a function
+            senderResp(xhr.responseText);
+        }
+    };
+    xhr.send();
+    return true; //keep channel open
 });
