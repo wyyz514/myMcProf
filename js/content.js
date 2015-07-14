5d8c430bc7c44173e57282f544880b6e92d3ae12
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-07-14 12:47:45 -0400

    Removed functions and added tem to app.content

--- js/content.js
+++ js/content.js
@@ -1,51 +1,7 @@
-var indices = {};
-
-indices.profCell = 16;
-indices.courseName = 2;
-indices.courseNumber = 3;
-
 chrome.runtime.onMessage.addListener(function(msg,sender,senderResp){
     console.log(msg.message);
     return true;
 });
 
-function makeProfNamesClickable()
-{
-    var profRows = document.querySelectorAll("tr td.dddefault");
-    var _profRows = Array.prototype.slice.call(profRows);
-    var regex = new RegExp(/^[a-z A-Z'-.]+$/);
-    for(var index = 0; index < _profRows.length; index++)
-    {
-        (function(i){
-            _profRows[i].addEventListener("click",function(e){
-                if(regex.exec(e.target.innerHTML) && e.target.innerHTML !== "TBA")
-                    findProfessor(e.target.innerHTML,sendMessage);
-            });
-        })(index);
-    }
-        
-}
 
-function findProfessor(names,callback)
-{
-    console.log(names);
-    if(names.search(",") > -1)
-    {
-        console.log("Multiple names");
-    }
-    else
-    {
-        callback(names);
-    }
-}
-
-function sendMessage(name)
-{
-    console.log("Sending message");
-    chrome.runtime.sendMessage({query:name,
-                                type:"PROF"},function(response){
-        console.log(response.message);
-    });
-}
-
-makeProfNamesClickable();
+app.content.makeProfNamesClickable();
