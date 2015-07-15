b6cc434ac7df1ec4925c9771aff7f0b1f25d1010
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-07-15 19:14:51 -0400

    Since makeRequest was moved to app, no need for app.background as context. New context is app

--- js/content.js
+++ js/content.js
@@ -1,14 +1,28 @@
 chrome.runtime.onMessage.addListener(function(msg,sender,senderResp){
-    app.profDetails.toggleLoad.call(this);
-    app.profDetails.addRatings(msg.message);
+    content.profDetails.toggleLoad();
+    content.profDetails.addRatings(msg.message);
     return true;
 });
 
-app.content.makeProfNamesClickable();
+content.makeProfNamesClickable();
 
-app.background.makeRequest(chrome.extension.getURL("../templates/card.html")).then(function(response){
+app.makeRequest(chrome.extension.getURL("../templates/card.html")).then(function(response){
     var divContainer = document.createElement("div");
     divContainer.classList.add("container");
     divContainer.innerHTML = response;
     document.body.appendChild(divContainer);
+    
+    document.querySelector("#search").addEventListener("keydown",function(e){
+        var el = e.target;
+        console.log(e.keyCode);
+        if(e.keyCode == 13)
+        {
+            content.sendMessage({
+                type:"PROF",
+                query:el.value
+            });
+        }
+    });
 });
+
+app.contentContext = this;
