abb1f6e85734c6ae3563514af3d70520522fa07b
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-07-16 18:08:48 -0400

    Tweaking input field's box shadow on focus

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded",function(){
+    var regex = new RegExp(/^[a-zA-Z'-]+/); //regex to avoid whitespace entries and special symbols
     var field = document.getElementById("popup-field");
     console.log(field);
     field.focus();
     field.addEventListener("keyup",function(e){
         console.log(e);
         var el = e.target;
-        if(e.keyCode == 13 && el.value != "") //enter key was pressed
+        if(e.keyCode == 13 && el.value !== "" && regex.exec(el.value)) //enter key was pressed
         {
             chrome.tabs.query({active:true,currentWindow:true},function(tabs){
                chrome.tabs.sendMessage(tabs[0].id,{type:"POPUP",query:el.value},function(response){
