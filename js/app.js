72d7055212390e6dfd27caf57fa173cb4fe6f3b3
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-07-16 16:01:14 -0400

    Nothing significant

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var app = app || {};
 
 app.makeRequest  = function(url)
 {
-    console.log("Making request");
     var promise = new Promise(function(resolve,reject){
         var xhr = new XMLHttpRequest();
         xhr.open("GET",url,true);
