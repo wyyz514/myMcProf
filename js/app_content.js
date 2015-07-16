cd038065eae46cb9b8a7043976bbea0d80b81f74
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-07-16 19:04:45 -0400

    Changing selector names

--- js/app_content.js
+++ js/app_content.js
@@ -67,7 +67,6 @@ var content = (function(){
             if(load.style.display.trim() == "none" || load.style.display == "")
             {
                 toggle = "on";
-                console.log("Loading is now on");
                 container.style.display = "block";
                 card.style.display = "block";
                 load.style.display = "block";
@@ -76,7 +75,6 @@ var content = (function(){
             if(load.style.display.trim() == "block")
             {
                 toggle = "off";
-                console.log("Loading is now off");
                 load.style.display = "none";
                 return;
             }
@@ -104,12 +102,13 @@ var content = (function(){
         showError:function(error)
         {
             document.querySelector(".mcprof-error").style.display = "block";
-            document.querySelector("#text").innerText = error;
+            document.querySelector("#mcprof-text").innerText = error;
         },
         clearError:function()
         {
             document.querySelector(".mcprof-error").style.display = "none";
-            document.querySelector("#text").innerText = "";
+            document.querySelector("#mcprof-text").innerText = "";
+            document.querySelector("#mcprof-search").value = "";
         }
     }
     
