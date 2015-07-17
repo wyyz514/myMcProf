add7d424ff08f200563987392c97c69fe7595e6a
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-07-17 13:53:48 -0400

    Moved capitalize function here from app_background

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 var app = app || {};
 
+app.capitalize = function(name)
+{
+    var chunks = name.split(" ");
+    for(var index = 0; index < chunks.length; index++)
+    {
+        chunks[index]  = chunks[index].charAt(0).toUpperCase() + chunks[index].slice(1);
+    }
+    var capitalizedName = chunks.join(" ");
+
+    //hypen in name
+    if(capitalizedName.indexOf("-") > 0)
+    {
+        var index = capitalizedName.indexOf("-");
+        capitalizedName = capitalizedName.replace(capitalizedName[index + 1],capitalizedName[index + 1].toUpperCase());
+    }
+    return capitalizedName;
+}
 app.makeRequest  = function(url)
 {
     var promise = new Promise(function(resolve,reject){
