218d44777c076ff024808ff67db4b2daf916c478
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-07-18 18:33:31 -0400

    Page action should only show on mcgill domains not on sites like my-mcgill.ca

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 //display icon when navigating through McGill domains
 chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
-    if(tab.url.search("mcgill.ca") > -1)
+    if(tab.url.search(".mcgill.ca") > -1)
     {
         chrome.pageAction.show(tabId);
     }
