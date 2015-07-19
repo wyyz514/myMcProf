04e11f399871b0357205f2c67271730c9b8925a5
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-07-19 15:26:02 -0400

    Added listener to buttons.

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded",function(){
-    var regex = new RegExp(/^[a-zA-Z'-]+/); //regex to avoid whitespace entries and special symbols
+    var regex = new RegExp(/[a-zA-Z'-]+/); //regex to avoid whitespace entries. Essentially will check for a first name
     var field = document.getElementById("popup-field");
-    console.log(field);
     field.focus();
-    field.addEventListener("keyup",function(e){
-        console.log(e);
-        var el = e.target;
-        if(e.keyCode == 13 && el.value !== "" && regex.exec(el.value)) //enter key was pressed
+    var searchButton = document.getElementById("popup-search");
+    searchButton.addEventListener("click",function(){
+        var fieldValue = field.value.trim();
+        if(fieldValue && regex.test(fieldValue))
         {
+            console.log(regex.exec(fieldValue));
             chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-               chrome.tabs.sendMessage(tabs[0].id,{type:"POPUP",query:el.value},function(response){
-                                       
-                }); 
+                chrome.tabs.sendMessage(tabs[0].id,{query:fieldValue,type:"POPUP",action:"SEARCH"},function(response){
+                    console.log(response);
+                });
             });
         }
     });
